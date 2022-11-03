65f75d4d2517a546bbe63fad5098a92b8e0271d1
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-11-03 19:34:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ valid = 0
 not_valid = 0
 
 st.sidebar.title("PDF Filter")
-input_pdf = st.sidebar.file_uploader(label="Upload PDF file(s):", type=['pdf'], accept_multiple_files=True)
+uploaded_files = st.sidebar.file_uploader(label="Upload PDF file(s):", type=['pdf'], accept_multiple_files=True)
 
 def load_model(): 
     reader = ocr.Reader(['en'],model_storage_directory='.')
@@ -21,28 +21,30 @@ def load_model():
 reader = load_model()
 
 def run():
-    pdfReader = PyPDF2.PdfFileReader(input_pdf)
-    pageObj = pdfReader.getPage(0) 
-    PDF_text = pageObj.extractText()
-    if "ACORD 25" not in PDF_text:
-        if len(PDF_text) == 0:
-            pil_image = pdf2image.convert_from_bytes(input_pdf.read())
-            result = reader.readtext(np.array(pil_image))
-            result_text = []
-            for text in result:
-                result_text.append(text[1])
-            final_text = " ".join([str(x) for x in result_text])
-            if "ACORD 25" not in final_text:
-                not_valid = not_valid + 1
+    for i in len(uploaded_files):
+        input_pdf = uploaded_files[i]
+        pdfReader = PyPDF2.PdfFileReader(input_pdf)
+        pageObj = pdfReader.getPage(0) 
+        PDF_text = pageObj.extractText()
+        if "ACORD 25" not in PDF_text:
+            if len(PDF_text) == 0:
+                pil_image = pdf2image.convert_from_bytes(input_pdf.read())
+                result = reader.readtext(np.array(pil_image))
+                result_text = []
+                for text in result:
+                    result_text.append(text[1])
+                final_text = " ".join([str(x) for x in result_text])
+                if "ACORD 25" not in final_text:
+                    not_valid = not_valid + 1
+                else:
+                    valid = valid + 1
             else:
-                valid = valid + 1
+                not_valid = not_valid + 1
         else:
-            not_valid = not_valid + 1
-    else:
-        valid = valid + 1
-    st.write("")
-    st.write("Valid:",valid)
-    st.write("Not valid", not_valid)
+            valid = valid + 1
+        st.write("")
+        st.write("Valid:",valid)
+        st.write("Not valid", not_valid)
     
 if st.sidebar.button("Run filtering"):
     run()
