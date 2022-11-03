a566215d89e7af599e315d7f8b7a89e01ac17871
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-11-03 19:16:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import easyocr as ocr  #OCR
 
 reader = ocr.Reader(['en'],model_storage_directory='.')
@@ -5,4 +6,4 @@ reader = ocr.Reader(['en'],model_storage_directory='.')
 result = reader.readtext("https://raw.githubusercontent.com/pc3541/PDF-Filter/main/outliers.jpeg")
 
 for text in result:
-    print(text[1])
+    st.write(text[1])
