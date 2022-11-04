897eca5a6e11535ab3ee955c8bc9f4408e675230
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-11-04 11:44:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 import streamlit as st
 import PyPDF2 
 import pdf2image
+from pdf2image import convert_from_bytes
 from PIL import Image
 import easyocr as ocr
 import numpy as np
-from pathlib import Path
 
 valid = 0
 not_valid = 0
@@ -24,28 +24,25 @@ st.title("Documents:")
 
 def run():
     for i in range(len(uploaded_files)):
-        
         input_pdf = uploaded_files[i]
         pdfReader = PyPDF2.PdfFileReader(input_pdf)
         pageObj = pdfReader.getPage(0) 
         PDF_text = pageObj.extractText()
         if "ACORD 25" not in PDF_text:
             if len(PDF_text) == 0:
-                try:
-                    pil_image = pdf2image.convert_from_path(Path(input_pdf.name), poppler_path="https://github.com/pc3541/PDF-Filter/tree/main/bin")
-                    result = reader.readtext(np.array(pil_image))
-                    result_text = []
-                    for text in result:
-                        result_text.append(text[1])
-                    final_text = " ".join([str(x) for x in result_text])
-                    if "ACORD 25" not in final_text:
-                        st.write(input_pdf.name, "**(bogus)**")
-                    else:
-                        st.write(input_pdf.name, "(valid)")
-                except:
-                    st.write("**Error analyzing **", input_pdf.name)
+                pdf_image = input_pdf.getvalue()
+                pil_image = pdf2image.convert_from_bytes(pdf_image)
+                result = reader.readtext(np.array(pil_image))
+                result_text = []
+                for text in result:
+                    result_text.append(text[1])
+                final_text = " ".join([str(x) for x in result_text])
+                if "ACORD 25" not in final_text:
+                    st.write(input_pdf.name, "(bogus)")
+                else:
+                    st.write(input_pdf.name, "(valid)")
             else:
-                st.write(input_pdf.name, "**(bogus)**")
+                st.write(input_pdf.name, "(bogus)")
         else:
             st.write(input_pdf.name, "(valid)")
     
