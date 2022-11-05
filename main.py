37e25ce2b8becd3e1fbb9e8780c0400b7c7bfc78
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-11-05 14:55:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,16 +27,18 @@ def run():
         pdfReader = PyPDF2.PdfReader(file)
         if len(pdfReader.pages[0].extractText()) == 0:
             bytes_data = file.read()
-            image = Image.open(io.BytesIO(bytes_data))
-            result = reader.readtext(np.array(pix))
-            result_text = []
-            for text in result:
-                result_text.append(text[1])
-            final_text = " ".join([str(x) for x in result_text])
-            if "ACORD 25" not in final_text:
-                st.write(file.name, " page ", page, " **(bogus)**")
-            else:
-                st.write(file.name, " page ", page, "(valid)")
+            with fitz.open(stream=bytes_data) as doc:
+                for page in doc:
+                    pix = page.get_pixmap()
+                    result = reader.readtext(np.array(pix))
+                    result_text = []
+                    for text in result:
+                        result_text.append(text[1])
+                    final_text = " ".join([str(x) for x in result_text])
+                    if "ACORD 25" not in final_text:
+                        st.write(file.name, " page ", page, " **(bogus)**")
+                    else:
+                        st.write(file.name, " page ", page, "(valid)")
         else:
             for pg in range(len(pdfReader.pages)):
                 pageObj = pdfReader.pages[pg]
